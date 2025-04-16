015d8b3aeb03be5e492ad57ca82fd13c4cfaf5f3
Borris <borris.wiria@gmail.com>
2025-04-16 15:24:21 +0400

    tsdown

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -3,34 +3,14 @@ import { defineConfig } from 'tsdown';
 import path from 'node:path';
 import postcss from 'rollup-plugin-postcss';
 import tailwindcss from '@tailwindcss/postcss';
-// import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    // css: './src/App.css',
-  },
-  // entry: ['./src', './src/App.css'],
-  // exclude: ['./src/paraglide/**'],
+  entry: ['src/**/*'],
 
-  // fromVite: true,
-
-  // sourcemap: true,
   clean: true,
   dts: {
     ignoreErrors: true,
-    // include: ['./src/**'],
-    exclude: ['./src/App.css'],
-
-    sourceMap: true,
-  },
-
-  outputOptions: {
-    // cssEntryFileNames: 'App.css',
-    // moduleTypes: { '.css': 'css' },
-  },
-  inputOptions: {
-    // moduleTypes: { '.css': 'css' },
+    include: ['src/**/*'],
   },
 
   sourcemap: true,
@@ -39,13 +19,15 @@ export default defineConfig({
   treeshake: true,
   outDir: './dist',
 
-  // bundleDts: true,
-
   alias: {
     '@': path.resolve(__dirname, './src'),
   },
 
   plugins: [
+    postcss({
+      extract: true,
+      plugins: [tailwindcss],
+    }),
     paraglideRolldownPlugin({
       project: './project.inlang',
       outdir: './src/paraglide',
@@ -54,26 +36,13 @@ export default defineConfig({
         {
           pattern:
             ':protocol://:domain(.*)::port?/cms/:locale(de|en)?/:path(.*)?',
-          // the original URL is https://example.com/about.
-          // hence, the locale is null
           deLocalizedNamedGroups: { locale: null },
           localizedNamedGroups: {
-            // the en locale should have no locale in the URL
-            // hence, the locale is null
             en: { locale: null },
-            // the de locale should have the locale in the URL
             de: { locale: 'de' },
           },
         },
       ],
     }),
-
-    postcss({
-      extract: true,
-      plugins: [tailwindcss()],
-    }),
-    // tailwindcss(),
   ],
-
-  // tsconfigPath: 'tsconfig.json',
 });
