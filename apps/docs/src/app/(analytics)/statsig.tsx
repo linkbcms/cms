a7b6d750869d9c0380da83210b50e505d3bcc341
Borris <borris.wiria@gmail.com>
2025-04-20 20:41:25 +0400

    Update statsig.tsx

--- apps/docs/src/app/(analytics)/statsig.tsx
+++ apps/docs/src/app/(analytics)/statsig.tsx
@@ -7,9 +7,11 @@ import { StatsigProvider, useClientAsyncInit } from '@statsig/react-bindings';
 import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
 import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
 
+const sdkKey = process.env.NEXT_PUBLIC_STATSIG_SDK_KEY ?? '';
+
 export default function Statsig({ children }: { children: React.ReactNode }) {
   const { client } = useClientAsyncInit(
-    'client-Ecn4kdJzFgxpeYkSPHjYGKHRAhIrDNWZ3Gl338nRU2V',
+    sdkKey,
     { userID: 'a-user' },
     {
       plugins: [
@@ -18,6 +20,10 @@ export default function Statsig({ children }: { children: React.ReactNode }) {
       ],
     },
   );
+
+  if (sdkKey === '') {
+    return <>{children}</>;
+  }
 
   return (
     <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
